70dd84683a22c5922750f4c9cef1fa4bed4c2a18
gdevcodes <gprog@tutanota.com>
2023-08-24 11:49:02 +0200

    intento finalizado

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,93 @@
-import Image from 'next/image'
+import Link from 'next/link';
+import { BiRegistered } from 'react-icons/bi';
+import proyectos from './datosProyectos.json';
+import Proyecto from './components/proyecto';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main className="px-3 flex flex-col gap-14 items-center">
+      {/* INICIO */}
+      <div className="flex flex-col gap-8">
+        <h1 className="text-[10vw] font-semibold">
+          <span className="flex">INMOBILIZE&reg;</span>
+          <span className="flex">DIVIDIMOS</span>
+
+          <span className="flex"> PARA INVERTIR</span>
+        </h1>
+        <p>
+          ¡Bienvenido a Inmobalize! El lugar idóneo para convertirte en inversor inmobiliario{' '}
+          <span className="font-semibold underline">desde 100€</span>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <Link href={'/'} className="underline hover:underline-offset-2">
+          Ver todos los proyectos
+        </Link>
+      </div>
+
+      {/* REGÍSTRATE */}
+      <div className="bg-welcome h-96 bg-cover bg-no-repeat w-full">
+        <div className="flex flex-col items-center justify-between py-14 bg-white h-full z-50 bg-opacity-80">
+          <div className="z-10 text-center">
+            <p className="mb-5">
+              BASTA SÓLO CON REGISTRARTE Y PASAR EL KYC <br /> PARA COMENZAR A INVERTIR EN
+              INMUEBLES.
+            </p>
+            <p>¡POTENCIA TUS FINANZAS CON NUESTRA PLATAFORMA!</p>
+          </div>
+          <button className="bg-black text-white px-8 py-2 rounded-xl w-48">Regístrate</button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* ANUNCIO */}
+      <p className="text-4xl ">DIVIDIR PARA INVERTIR</p>
+      {/* ULTIMOS PROYECTOS */}
+      <div className="bg-black  w-full text-white px-4 flex flex-col items-center max-w-6xl">
+        <h2 className="py-10 self-start text-4xl">
+          Últimos <br /> proyectos
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+          {proyectos.map((proyecto) => (
+            <Proyecto
+              imagen={proyecto.imagen}
+              ubicacion={proyecto.ubicacion}
+              inversionTotal={proyecto.inversionTotal}
+              precioPorNft={proyecto.precioPorNft}
+              duracionExplotacion={proyecto.duracionExplotacion}
+              inicioRenta={proyecto.inicioRenta}
+              rentabilidadMediaAnual={proyecto.rentabilidadMediaAnual}
+            />
+          ))}
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* PRIMEROS PASOS */}
+      <div className="h-screen flex flex-col w-full px-4">
+        <h2 className="justify-self-start text-4xl">Primeros pasos</h2>
+        <p>¿Listo para invertir en inmuebles? ¡Estos son los pasos que debes de seguir!</p>
+        <div className="flex gap-2 mt-10 w-full justify-evenly">
+          <div className="flex flex-col items-center">
+            <p className="hidden">REGÍSTRATE</p>
+            <p className="text-4xl">01</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="hidden">HAZ TU KYC</p>
+            <p className="text-4xl">02</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="hidden">SELECCIONA INVERSIÓN</p>
+            <p className="text-4xl">03</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="hidden">TUS PARTICIPACIONES</p>
+            <p className="text-4xl">04</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="hidden">LIQUIDEZ</p>
+            <p className="text-4xl">05</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="hidden">TUS BENEFICIOS</p>
+            <p className="text-4xl">06</p>
+          </div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
